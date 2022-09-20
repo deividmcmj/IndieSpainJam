3cff99ffe45a288e96b4749d65e10642a4e04b0f
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2022-09-20 18:23:55 +0200

    Objeto de mercancía de prueba

--- IndieSpainJam/Assets/Scripts/PlayerController.cs
+++ IndieSpainJam/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a98c215bc4f74ee127e23f2fb0f1480635c550bab29f2d70dee9d515acc88216
-size 1380
+oid sha256:af00c98e6cf1c9e2806d2e11974d88c3304e7417b57916e5dd8c100bca4da335
+size 2103
---
+++ IndieSpainJam/Assets/Scripts/Target.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe6f4567dedd56487741eeef875dc8ba2ee001c7ab8ebbd194839f9b7793e747
+size 734
